d4d39bd51e9462d5dd05897657088b593a1b1d76
Vedrana Loncar <vedrana333@gmail.com>
2023-06-26 12:52:45 +0200

    updated

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 
-#from dotenv import load_dotenv
 
+#from dotenv import load_dotenv
 from src.extract import extract_transaction_data
 from src.transform import identify_and_remove_duplicated_data
 from src.load_data_to_s3 import df_to_s3
-
 
 #load_dotenv()
 user = os.getenv("user")
@@ -26,9 +25,7 @@ print("Removed duplicated data")
 
 # load data to s3
 
-s3_bucket="waia-data-dump"
-key="bootcamp2/etl/vl_online_trans_cleaned.csv"
+s3_bucket = "waia-data-dump"
+key = "bootcamp2/etl/vl_online_trans_cleaned.csv"
 
 df_to_s3(online_trans_deduped, key, s3_bucket, aws_access_key_id, aws_secret_access_key_id)
-
-
